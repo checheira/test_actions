a50b8ffe78eda7db11e9ce726f9a8c50b536f41b
Iratxe <ibarreiro@iberdrola.com>
2024-10-23 14:26:52 +0200

    retry

--- scripts/python_file_example.py
+++ scripts/python_file_example.py
@@ -6,5 +6,5 @@ hoy = date.today()
 
 # Imprimir la fecha de hoy
 print("La fecha de hoy es:", hoy)
-print(os.getenv('nosec_value', "unknown"))
-print(os.getenv('sec_value', "unknown"))
+print(os.environ.get("nosec_value", "unknown"))
+print(os.getenv("sec_value", "unknown"))
