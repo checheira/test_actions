7597902b6402ad4d61ef51a91f15db8a347b0ef1
Iratxe <ibarreiro@iberdrola.com>
2024-10-23 14:08:40 +0200

    try to add variables in python

--- scripts/python_file_example.py
+++ scripts/python_file_example.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 
 # Obtener la fecha de hoy
@@ -5,3 +6,5 @@ hoy = date.today()
 
 # Imprimir la fecha de hoy
 print("La fecha de hoy es:", hoy)
+print(f'Name is {os.environ.get("NO_SEC_VARIABLE", "<unknown")}')
+print(f'PASS is {os.environ.get("SECRET_VALUE", "<unknown")}')
