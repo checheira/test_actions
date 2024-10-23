d8f033065a44b0bf9fb4645d51783668eba60899
Iratxe <ibarreiro@iberdrola.com>
2024-10-23 14:19:43 +0200

    creo variable de ENTORNO

--- scripts/python_file_example.py
+++ scripts/python_file_example.py
@@ -6,7 +6,7 @@ hoy = date.today()
 
 # Imprimir la fecha de hoy
 print("La fecha de hoy es:", hoy)
-print(f'Name is {os.getenv("NO_SEC_VARIABLE")}')
+print(f'Name is {os.getenv("ENV_VARIABLE1")}')
 print(f'PASS is {os.getenv("SECRET_VALUE")}')
-print(os.getenv('NO_SEC_VARIABLE', "unknown"))
+print(os.getenv('ENV_VARIABLE1', "unknown"))
 print(os.getenv('SECRET_VALUE', "unknown"))
